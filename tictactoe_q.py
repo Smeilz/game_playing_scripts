322a5794292d0e475f22bc48398a2823d09edce1
Neil Slater <neil@aframe.com>
2017-09-30 17:12:34 +0100

    Apache License boilerplate

--- tictactoe_q.py
+++ tictactoe_q.py
@@ -1,3 +1,19 @@
+'''
+   Copyright 2017 Neil Slater
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
 import numpy as np
 import random
 from itertools import groupby
@@ -61,7 +77,7 @@ class TicTacToeGame():
 
 
 class Agent():
-    def __init__(self, game_class, epsilon=0.1, alpha = 0.5, value_player='X'):
+    def __init__(self, game_class, epsilon=0.1, alpha=0.5, value_player='X'):
         self.V = dict()
         self.NewGame = game_class
         self.epsilon = epsilon
@@ -167,7 +183,7 @@ def demo_game_stats(agent):
     print("    percentage results: {}".format(game_stats))
 
 if __name__ == '__main__':
-    agent = Agent(TicTacToeGame, epsilon = 0.15, alpha = 1.0)
+    agent = Agent(TicTacToeGame, epsilon = 0.1, alpha = 1.0)
     print("Before learning:")
     demo_game_stats(agent)
 
